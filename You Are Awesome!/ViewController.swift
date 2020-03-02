2da245b198b9ac819fd045862dff0e5c7c507f43
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-03-01 22:00:37 -0800

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Huimin Jiang on 3/1/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
